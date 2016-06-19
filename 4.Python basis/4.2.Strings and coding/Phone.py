00d813475cdd24b553f5cfbc41b3f79e78dadc31
csnow798 <csnow798@sina.com>
2016-06-19 20:03:15 +0800

    Add file via upload 4.2. Phone credit

---
+++ 4.Python basis/4.2.Strings and coding/Phone.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+n=raw_input('Please input name:')
+l=raw_input('Please input money:')
+l=int(l)
+m=raw_input('Please input month:')
+print "Dear %s, your phone credit balance has left $%d in the month of %s. " % (n,l,m)
