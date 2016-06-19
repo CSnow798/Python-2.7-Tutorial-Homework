dcd082cf2fbacb4b248798a7e7d9c3e32fcef3da
csnow798 <csnow798@sina.com>
2016-06-19 17:21:33 +0800

    Add file via upload 4.Python basis

---
+++ 4.Python basis/Input_if.py
@@ -0,0 +1,8 @@
+#print absolute value of an integer:
+#input number:
+a=raw_input('Please input an integer:')
+a=int(a)
+if a>=0:
+	print 'The absolute value of',a,'is',a
+else:
+	print 'The absolute value of',a,'is',-a
---
+++ 4.Python basis/Input_if_1.0.py
@@ -0,0 +1,7 @@
+#Input a number and print absolute value of that integer:
+a=raw_input('Please input an integer:')
+a=int(a)
+if a>=0:
+	print a
+else:
+	print -a
