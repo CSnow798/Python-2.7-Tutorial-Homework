57d1fb39a4977f7babc95f6f623dad51b7a2bf9e
csnow798 <csnow798@sina.com>
2016-06-20 10:09:05 +0800

    Add file via upload 4.4.

---
+++ 4.Python basis/4.4.Conditional and loop/Sum_1-10.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#Sum 1-10
+sum=0
+for x in [1,2,3,4,5,6,7,8,9,10]:
+	sum=sum+x
+print sum
---
+++ 4.Python basis/4.4.Conditional and loop/Sum_odd_within_100.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#Sum all the odd number within 100
+sum=0
+n=99
+while n>0:
+	sum=sum+n
+	n=n-2
+print sum
